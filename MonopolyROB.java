e78f2fcebfa144fbfb764232494222bed7ea8327
RobertMetcalfe10 <robert.metcalfe@ucdconnect.ie>
2017-02-07 16:18:49 +0000

    Add files via upload

---
+++ MonopolyROB.java
@@ -0,0 +1,55 @@
+package Monopoly;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.text.DefaultCaret;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JInternalFrame;
+import javax.swing.JTextArea;
+import javax.swing.JEditorPane;
+import javax.swing.JSpinner;
+import java.util.*;
+
+
+
+public class MonopolyROB extends Switch{
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run() {
+				try {
+					MonopolyROB window = new MonopolyROB();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MonopolyROB() {
+		initialize();
+	}
+
+	
+}
---
+++ Switch.java
@@ -0,0 +1,94 @@
+package Monopoly;
+
+public class Switch extends UI
+{
+	
+	void switchUser (int User)
+	{
+	
+	switch(User)
+	{
+    case 1:UI.lblA.setBounds(614, 661, 10, 10);
+		break;
+	case 2:UI.lblA.setBounds(559, 661, 10, 10);
+		break;
+    case 3:UI.lblA.setBounds(505, 661, 10, 10);
+		break;
+	case 4:UI.lblA.setBounds(447, 661, 10, 10);
+		break;
+    case 5:UI.lblA.setBounds(387, 661, 10, 10);
+		break;
+	case 6:UI.lblA.setBounds(321, 661, 10, 10);
+		break;
+    case 7:UI.lblA.setBounds(292, 627, 10, 10);
+		break;
+	case 8:UI.lblA.setBounds(199, 661, 10, 10);
+		break;
+    case 9:UI.lblA.setBounds(179, 627, 10, 10);
+		break;
+	case 10:UI.lblA.setBounds(92, 661, 10, 10);
+		break;
+    case 11:UI.lblA.setBounds(25, 661, 10, 10);
+		break;
+	case 12:UI.lblA.setBounds(64, 569, 10, 10);
+		break;
+    case 13:UI.lblA.setBounds(64, 479, 10, 10);
+		break;
+	case 14:UI.lblA.setBounds(64, 439, 10, 10);
+		break;
+    case 15:UI.lblA.setBounds(25, 395, 10, 10);
+		break;
+	case 16:UI.lblA.setBounds(64, 308, 10, 10);
+		break;
+    case 17:UI.lblA.setBounds(66, 262, 10, 10);
+		break;
+	case 18:UI.lblA.setBounds(64, 216, 10, 10);
+		break;
+    case 19:UI.lblA.setBounds(25, 175, 10, 10);
+		break;
+	case 20:UI.lblA.setBounds(25, 111, 10, 10);
+		break;
+    case 21:UI.lblA.setBounds(92, 105, 10, 10);
+		break;
+	case 22:UI.lblA.setBounds(149, 111, 10, 10);
+		break;
+    case 23:UI.lblA.setBounds(227, 73, 10, 10);
+		break;
+	case 24:UI.lblA.setBounds(253, 111, 10, 10);
+		break;
+    case 25:UI.lblA.setBounds(321, 111, 10, 10);
+		break;
+	case 26:UI.lblA.setBounds(417, 73, 10, 10);
+		break;
+    case 27:UI.lblA.setBounds(447, 111, 10, 10);
+		break;
+	case 28:UI.lblA.setBounds(532, 73, 10, 10);
+		break;
+    case 29:UI.lblA.setBounds(549, 111, 10, 10);
+		break;
+	case 30:UI.lblA.setBounds(614, 111, 10, 10);
+		break;
+    case 31:UI.lblA.setBounds(647, 146, 10, 10);
+		break;
+	case 32:UI.lblA.setBounds(614, 244, 10, 10);
+		break;
+    case 33:UI.lblA.setBounds(636, 270, 10, 10);
+		break;
+	case 34:UI.lblA.setBounds(636, 315, 10, 10);
+		break;
+    case 35:UI.lblA.setBounds(647, 360, 10, 10);
+		break;
+	case 36:UI.lblA.setBounds(614, 455, 10, 10);
+		break;
+    case 37:UI.lblA.setBounds(636, 488, 10, 10);
+		break;
+	case 38:UI.lblA.setBounds(650, 525, 10, 10);
+		break;
+    case 39:UI.lblA.setBounds(647, 569, 10, 10);
+		break;
+	case 40:UI.lblA.setBounds(25, 111, 10, 10);
+		break;
+	}
+	}
+
+}
