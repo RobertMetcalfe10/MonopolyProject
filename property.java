c15a53103aa4cce53afeb8a249b9c8c74fa76292
erikburka <erikburkasrfc@gmail.com>
2017-02-17 15:15:42 +0000

    Update to property

--- property.java
+++ property.java
@@ -1,6 +1,6 @@
 package Monopoly;
 
-public class Property extends Variables{
+public class Property extends BankAccount{
 	
 	protected static int propertyID;
 	protected static String propertyName;
@@ -9,10 +9,10 @@ public class Property extends Variables{
 	protected static int housePrice;
 	protected static int mortgageValue;
 	protected static String propertyColour;
-	protected static boolean propertyOwned;
-	protected static int propertyHouse;
+	protected static boolean propertyOwned=true;
+	protected static int propertyHouseNo;
 	
-	void propertyDisplay (){
+	String propertyDisplay (){
 		
 		if (propertyOwned == false){
 			propertyDets += "You don't own any property";
@@ -22,17 +22,26 @@ public class Property extends Variables{
 								"Property Colour - " + propertyColour + "\n" +
 								"Property Price - " + propertyPrice + "\n";	
 		}
+		return propertyDets;
 	}
 	void buyProperty(){
-		
+		propertyNo++;
+		BankAccount WD=new BankAccount();
+		WD.withdraw(propertyPrice);
 	}
 	
 	void payRent(){
+		BankAccount WD=new BankAccount();
+		WD.withdraw(propertyRent);
 		
+		//Change user
+		WD.deposit(propertyRent);
 	}
 	
 	void buildHouse(){
-		
+		propertyHouseNo++;
+		BankAccount WD=new BankAccount();
+		WD.withdraw(housePrice);
 	}
 	
 	
