794770bc682cb5bf540f5596c2997352ea776ac9
VladUkhatyuk <vladukhatyuk@gmail.com>
2017-02-20 16:13:09 +0000

    Added Block info

--- Variables.java
+++ Variables.java
@@ -1,5 +1,7 @@
 package Monopoly;
 
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
@@ -12,6 +14,7 @@ public class Variables {
 	protected JFrame frame;
 	protected JTextField textField;
 	protected static String command="";
+	protected static String propertyDets ="";
 	protected static int UserA=1;
 	protected static int UserB=1;
 	protected static int UserC=1;
@@ -25,8 +28,8 @@ public class Variables {
 	protected static String UserNameY;
 	protected static String UserNameZ;
 	protected static String print;
-	protected static double display;
 	protected static int randomNum;
+	protected static int colour;
 	protected static int UserChooser=1;
 	protected static int NumOfUsers=1;
 	protected static JLabel lblA = new JLabel("A");
@@ -35,22 +38,58 @@ public class Variables {
 	protected static JLabel lblX = new JLabel("X");
 	protected static JLabel lblY = new JLabel("Y");
 	protected static JLabel lblZ = new JLabel("Z");
-	protected static JTextPane txtpnItIsNow = new JTextPane();
+	protected static JTextPane txtpnInfo = new JTextPane();
 	protected static JButton btnRollDice = new JButton("Roll Dice");
 	protected static JInternalFrame HelpFrame = new JInternalFrame("List Of Commands");
-	protected static BankAccount BankAccountUserA = new BankAccount(1500);
-	protected static BankAccount BankAccountUserB = new BankAccount(2500);
-	protected static BankAccount BankAccountUserC = new BankAccount(3500);
-	protected static BankAccount BankAccountUserX = new BankAccount(4500);
-	protected static BankAccount BankAccountUserY = new BankAccount(5500);
-	protected static BankAccount BankAccountUserZ = new BankAccount(6500);
+	protected static JInternalFrame PropertyFrame = new JInternalFrame("Your Property Details");
+	// http://www.java2s.com/Tutorial/Java/0240__Swing/CustomizingaJInternalFrameLookandFeel.htm //
+	protected static ArrayList<Property> properties = new ArrayList<Property>();
+	protected static Property OldKentRd=new Property();
 	
+	// (BlockID,BlockName,BlockPrice,BlockRent)
 	
-	
-	
-	
-	
-	
-	
-	
+
+//	properties.add(Property(1,Start,null,{2,10,30,90,160,250},))
+//	properties.add(Property(2,OldKentRd,60,{4,20,60,180,320,450},))
+//	properties.add(Property(3,CommunityChest,null,))
+//	properties.add(Property(4,WhiteChapelRoad,60,{6,30,90,270,400,550},))
+//	properties.add(Property(5,IncomeTax,null,))
+//	properties.add(Property(6,KingsCrossStation,null,))
+//	properties.add(Property(7,TheAngelIslington,100,{6,30,90,270,400,550},))
+//	properties.add(Property(8,Chance,null,))
+//	properties.add(Property(9,EustonRoad,100,{8,40,100,300,450,600},))
+//	properties.add(Property(10,PentovilleRoad,120,{10,50,150,450,625,750},))
+//	properties.add(Property(11,Jail,null,))
+//	properties.add(Property(12,PallMall,140,{10,50,150,450,625,750},))
+//	properties.add(Property(13,ElectricCompany,null,))
+//	properties.add(Property(14,WhiteHall,140,{12,60,180,500,700,900},))
+//	properties.add(Property(15,NorthumberlandAvenue,160,{14,70,200,550,750,950},))
+//	properties.add(Property(16,MaryleboneStation,null,))
+//	properties.add(Property(17,BowStreet,180,{25,50,100,200,200,200},))
+//	properties.add(Property(18,CommunityChest,null,))
+//	properties.add(Property(19,MarlboroughStreet,180,{14,70,200,550,750,950},))
+//	properties.add(Property(20,VineStreet,200,{16,80,220,600,800,1000},))
+//	properties.add(Property(21,FreeParking,null,))
+//	properties.add(Property(22,Strand,220,{18,90,250,700,875,1050},))
+//	properties.add(Property(23,Chance,null,))
+//	properties.add(Property(24,FleetSteet,220,{4,10,0,0,0,0},))
+//	properties.add(Property(25,TrafalgarSquare,240,,{18,90,250,700,875,1050}))
+//	properties.add(Property(26,FrenchurchStStation,null,))
+//	properties.add(Property(27,LeicesterSquare,260,{20,100,300,750,925,1100},))
+//	properties.add(Property(28,CoventrySteet,260,{22,110,330,800,975,1150},))
+//	properties.add(Property(29,WaterWorks,null,))
+//	properties.add(Property(30,Piccadilly,280,{22,110,330,800,975,1150},))
+//	properties.add(Property(31,Go2Jail,null,))
+//	properties.add(Property(32,RegentStreet,300,{22,120,360,850,1025,1200},))
+//	properties.add(Property(33,OxfordStreet,300,{26,130,390,900,1100,1275},))
+//	properties.add(Property(34,CommunityChest,null,))
+//	properties.add(Property(35,BondStreet,320,{28,150,450,1000,1200,1400},))
+//	properties.add(Property(36,LiverpoolStreetStation,null,))
+//	properties.add(Property(37,Chance,null,))
+//	properties.add(Property(38,ParkLane,350,{35,175,500,1100,1300,1500},))
+//	properties.add(Property(49,Supertax,null,))
+//	properties.add(Property(40,Mayfair,400,{40,200,550,2100,1400,1600}))
+
+
 }
+
